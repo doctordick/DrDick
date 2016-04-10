0574b2688ed71d4778a5fc029f181279e27a7abb
Sharmila Jesupaul <tosharmila@gmail.com>
2016-04-09 21:57:30 -0700

    adds questionnnaire state change

--- App/Components/Question.js
+++ App/Components/Question.js
@@ -2,23 +2,62 @@ import React, {
   AppRegistry,
   Component,
   Text,
+  StyleSheet,
   View,
   TouchableHighlight,
   Navigator
 } from 'react-native';
 
+import Header from './Header';
+
 class Question extends Component {
-  nextRoute() {
-    console.log('helloo');
+  nextQuestion() {
+    this.props.nextQuestion();
+  }
+
+  generateOptions() {
+    let context = this;
+    const options = [
+      "Less than 1 month ago", 
+      "1 to 3 months ago",
+      "3 to 6 months ago ",
+      "More than 6 months ago"
+    ];
+
+    return options.map((option, index)=>{
+      return (
+        <TouchableHighlight onPress={context.nextQuestion.bind(context)} key={index}>
+          <View>
+            <Text>
+              {option}
+            </Text>
+          </View>
+        </TouchableHighlight> 
+      );
+    });
   }
 
   render() {
     return (
-      <View>
-      
+      <View style={styles.container}>
+        <Header value={this.props.header} />
+        <Text>
+          {this.props.question}
+        </Text>
+        {this.generateOptions()}
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
 
 export default Question
--- App/Components/Questionnaire.js
+++ App/Components/Questionnaire.js
@@ -9,28 +9,79 @@ import React, {
   Navigator
 } from 'react-native';
 
+import Header from './Header';
+import Question from './Question';
+
+const questionCollection = [{
+    header: 'Your Recent HIV & STD Tests',
+    questions: [
+        "Last HIV test? ",
+        "Last syphilis test?",
+        "Last urine test for GC and CT?",
+        "Last throat swab for GC and CT?",
+        "Last butt swab for GC and CT?",
+        "Last herpes type 2 test?",
+        "Last hepatitis C test?"
+    ]
+}, {
+    header: "Your Recent Sexual History",
+    questions: [
+        "Last time bottoming?",
+        "Last time topping?",
+        "Last time giving a blow job?",
+        "Last time getting a blow job?",
+        "Last time injecting recreational drugs (meth or anything else)?"
+    ]
+}]
+
 class Questionnaire extends Component {
-  nextRoute() {
-    console.log('helloo');
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            header: questionCollection[0].header,
+            question: questionCollection[0].questions[0],
+            questionIndex: 0,
+            collectionIndex: 0
+        };
+    }
+    switchQuestion() {
+        // increment number until the end of first array
+        // then switch to the second array
+        this.state.questionIndex++;
+        let questionIndex = this.state.questionIndex; 
+        let collectionIndex = this.state.collectionIndex;
 
-  render() {
-    return (
-        <Navigator
-          renderScene={this.renderScene.bind(this)}
-        />
-    );
-  }
+        if(collectionIndex === 0 && questionIndex >= 7) {
+            collectionIndex = this.state.collectionIndex++;
+            questionIndex = 0;
+        } 
 
-  renderScene(route, navigator){
-    return (
-      <View style={styles.container}>
-        <Text>
-          Questionnaire
-        </Text> 
-      </View>
-    );
-  }
+        if(collectionIndex === 1 && questionIndex >= 5){
+            // This route change isn't working
+            // this.props.nextRoute({
+            //    id: "Loading" 
+            // }).bind(this);
+        }
+
+        this.setState({ question: questionCollection[collectionIndex].questions[questionIndex] });
+    }
+
+    render() {
+        return (
+            <Navigator
+              renderScene={this.renderScene.bind(this)}
+            />
+        );
+    }
+
+    renderScene(route, navigator){
+        return (
+          <Question header={this.state.header} 
+                    question={this.state.question} 
+                    nextQuestion={this.switchQuestion.bind(this)}
+                    />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
